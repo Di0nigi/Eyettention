a26aeb45956305bad790ba14bc5a732ecd48f717
Le Minh Truong <truongleminh2001@gmail.com>
2026-09-14 13:03:43 +0200

    updated init

--- __init__.py
+++ __init__.py
@@ -1,9 +1,27 @@
 # API implementation
 
 from Eyettention.model import Eyettention, Eyettention_readerID
+from Eyettention.utils import (
+    BSCdataset,
+    calculate_mean_std,
+    celerdataset,
+    eval_log_llh,
+    gradient_clipping,
+    load_corpus,
+    load_label,
+    prepare_scanpath,
+)
 
 
 __all__ = [
     'Eyettention',
-    'Eyettention_readerID'
+    'Eyettention_readerID',
+    'BSCdataset',
+    'calculate_mean_std',
+    'celerdataset',
+    'eval_log_llh',
+    'gradient_clipping',
+    'load_corpus',
+    'load_label',
+    'prepare_scanpath'
 ]
