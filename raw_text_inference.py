3065f504967627c0a39ca8a9ee77c1518a7847a8
Le Minh Truong <truongleminh2001@gmail.com>
2026-09-17 12:28:08 +0200

    renaming

--- raw_text_inference.py
+++ raw_text_inference.py
@@ -5,7 +5,7 @@ from Eyettention.model import Eyettention
 from Eyettention.utils import build_bsc_config, build_label_encoder, text_to_bsc_inputs
 
 
-class EyettentionPredictor:
+class EyettentionRawTextInference:
 	def __init__(self, checkpoint_path, cf=None, device="cpu"):
 		self.cf = cf or build_bsc_config()
 		self.device = device
@@ -31,32 +31,6 @@ class EyettentionPredictor:
 				sn_emd=sn_input_ids,
 				sn_mask=sn_mask,
 				word_ids_sn=None,
-				sn_word_len=sn_word_len,
-				le=self.label_encoder,
-				max_pred_len=max_pred_len or self.cf["max_pred_len"],
-			)
-
-	def generate_from_tokenized_text(
-		self,
-		sn_input_ids,
-		sn_mask,
-		sn_word_len,
-		word_ids_sn=None,
-		max_pred_len=None,
-	):
-		"""Generate from tokenised text."""
-		sn_input_ids = sn_input_ids.to(self.device)
-		sn_mask = sn_mask.to(self.device)
-		sn_word_len = sn_word_len.to(self.device)
-
-		if word_ids_sn is not None:
-			word_ids_sn = word_ids_sn.to(self.device)
-
-		with torch.no_grad():
-			return self.model.scanpath_generation(
-				sn_emd=sn_input_ids,
-				sn_mask=sn_mask,
-				word_ids_sn=word_ids_sn,
 				sn_word_len=sn_word_len,
 				le=self.label_encoder,
 				max_pred_len=max_pred_len or self.cf["max_pred_len"],
